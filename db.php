7e001dbb1373c79c7a2d71329a0243d351b34433
DishaKD21 <vdsh321@gmail.com>
2025-05-20 12:02:31 +0530

    changed db.php

--- db.php
+++ db.php
@@ -4,4 +4,13 @@ session_start();
 
 error_reporting(E_ALL);
 
-$conn = mysqli_connect('localhost', 'root', 'root', 'spotify_clone');
+$dbHost = "localhost";
+$dbUser = "root";         // Change for deployment
+$dbPass = "root";         // Change for deployment
+$dbName = "spotify_clone";
+
+$conn = new mysqli($dbHost, $dbUser, $dbPass, $dbName);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
